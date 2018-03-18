ec36fa9681a80d4663aeff2e3b377372ba63a181
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-18 11:31:53 +0700

    Add method in DbManager 1. getCategoryTransactionById 2. queryGetAllTransaction

--- app/src/main/java/com/ysn/dosist/db/DbManager.kt
+++ app/src/main/java/com/ysn/dosist/db/DbManager.kt
@@ -2,8 +2,12 @@ package com.ysn.dosist.db
 
 import android.app.Application
 import com.ysn.dosist.db.entity.BalanceCurrent
+import com.ysn.dosist.db.entity.CategoryTransaction
+import com.ysn.dosist.db.entity.DetailTransaction
+import com.ysn.dosist.db.entity.DetailTransaction_
 import com.ysn.dosist.views.base.App
 import io.objectbox.Box
+import io.objectbox.query.Query
 import javax.inject.Inject
 
 /**
@@ -12,11 +16,20 @@ import javax.inject.Inject
 class DbManager @Inject constructor(val application: Application) {
 
     private var balanceCurrentBox: Box<BalanceCurrent> = (application as App).boxStore.boxFor(BalanceCurrent::class.java)
+    private var categoryTransaction: Box<CategoryTransaction> = (application as App).boxStore.boxFor(CategoryTransaction::class.java)
+    private var detailTransaction: Box<DetailTransaction> = (application as App).boxStore.boxFor(DetailTransaction::class.java)
 
+    /**
+     * Put Balance Current Box
+     * @param balanceCurrent - Value balance current
+     */
     fun putBalanceCurrentBox(balanceCurrent: BalanceCurrent) {
         balanceCurrentBox.put(balanceCurrent)
     }
 
+    /**
+     * Query Balance Current Box
+     */
     fun queryBalanceCurrentBox(): BalanceCurrent {
         val countItem = balanceCurrentBox.count()
         return if (countItem == 0L) {
@@ -25,6 +38,23 @@ class DbManager @Inject constructor(val application: Application) {
         } else {
             balanceCurrentBox.query().build().findFirst()!!
         }
+    }
+
+    /**
+     * Get Category DetailTransaction by ID Category
+     * @param idCategory - Value ID Category
+     * @return Result value Category DetailTransaction
+     */
+    fun getCategoryTransactionById(idCategory: Long): CategoryTransaction {
+        return categoryTransaction.get(idCategory)
+    }
+
+    /**
+     * Get all DetailTransaction
+     * @return Result list DetailTransaction
+     */
+    fun queryGetAllTransaction(): Query<DetailTransaction> {
+        return detailTransaction.query().order(DetailTransaction_.id).build()
     }
 
 }
