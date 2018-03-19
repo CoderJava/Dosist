ae0cf327e92d284f91e120c7d49031640ddfcea4
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-20 02:23:12 +0700

    Add feature choose category transaction

--- app/src/main/java/com/ysn/dosist/views/ui/addtransaction/AddTransactionActivity.kt
+++ app/src/main/java/com/ysn/dosist/views/ui/addtransaction/AddTransactionActivity.kt
@@ -1,8 +1,8 @@
 /*
- * Created by YSN Studio on 3/20/18 12:24 AM
+ * Created by YSN Studio on 3/20/18 2:23 AM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 3/20/18 12:24 AM
+ * Last modified 3/20/18 2:11 AM
  */
 
 package com.ysn.dosist.views.ui.addtransaction
@@ -13,14 +13,17 @@ import android.support.v4.content.ContextCompat
 import android.view.View
 import com.jakewharton.rxbinding2.widget.RxTextView
 import com.ysn.dosist.R
+import com.ysn.dosist.db.entity.CategoryTransaction
 import com.ysn.dosist.db.entity.DetailTransaction
 import com.ysn.dosist.di.component.addtransaction.DaggerAddTransactionActivityComponent
 import com.ysn.dosist.di.module.addtransaction.AddTransactionActivityModule
 import com.ysn.dosist.views.base.BaseActivity
+import com.ysn.dosist.views.ui.choosecategory.ChooseCategoryBottomSheetDialogFragment
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_add_transaction.*
 import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.design.snackbar
 import org.jetbrains.anko.info
@@ -31,19 +34,37 @@ import javax.inject.Inject
 
 class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickListener, AnkoLogger {
 
+    private val TAG = javaClass.simpleName
+
     @Inject
     lateinit var presenter: AddTransactionPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_transaction)
+        registerEventBus()
         initListeners()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterEventBus()
+    }
+
+    private fun unregisterEventBus() {
+        EventBus.getDefault().unregister(this)
+    }
+
+    private fun registerEventBus() {
+        EventBus.getDefault().register(this)
+    }
+
 
     private fun initListeners() {
         text_view_income_activity_add_transaction.setOnClickListener(this)
         text_view_expense_activity_add_transaction.setOnClickListener(this)
         image_view_save_activity_add_transaction.setOnClickListener(this)
+        linear_layout_container_category_activity_add_transaction.setOnClickListener(this)
         RxTextView.textChanges(edit_text_amount_add_transaction_activity)
                 .debounce(500, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
@@ -85,6 +106,7 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
         when (view.id) {
             R.id.text_view_income_activity_add_transaction -> incomeViewActive()
             R.id.text_view_expense_activity_add_transaction -> expenseViewActive()
+            R.id.linear_layout_container_category_activity_add_transaction -> chooseCategory()
             R.id.image_view_save_activity_add_transaction -> {
                 val subjectTransaction = edit_text_subject_activity_add_transaction.text.toString()
                         .capitalize()
@@ -96,16 +118,17 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
                         .replace(".", "")
                         .toLong()
                 val category = text_view_category_activity_add_transaction.text.toString().toLowerCase()
+                val idCategory = if (category.equals(getString(R.string.choose_category), true)) 0L else text_view_category_activity_add_transaction.tag.toString().toLong()
                 // TODO: do something in here (pending)
 
                 val tagIncome = text_view_income_activity_add_transaction.tag
                 val typeTransaction = if (tagIncome == "selected") "income" else "expense"
-                info { "typeTransaction: $typeTransaction" }
+                info { "idCategory: $idCategory" }
                 val detailTransaction = DetailTransaction(
                         subject = subjectTransaction,
                         description = descriptionTransaction,
                         amount = amountTransaction,
-                        idCategory = 1L,
+                        idCategory = idCategory,
                         type = typeTransaction
                 )
                 presenter.onSaveTransaction(detailTransaction = detailTransaction)
@@ -114,6 +137,11 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
                 /** nothing to do in here */
             }
         }
+    }
+
+    private fun chooseCategory() {
+        val chooseCategoryBottomSheetDialogFragment = ChooseCategoryBottomSheetDialogFragment()
+        chooseCategoryBottomSheetDialogFragment.show(supportFragmentManager, TAG)
     }
 
     private fun expenseViewActive() {
@@ -146,5 +174,20 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
         mapData["fromClass"] = javaClass.simpleName
         EventBus.getDefault().post(mapData)
         finish()
+    }
+
+    @Subscribe
+    fun onMessageEvent(mapData: HashMap<String, Any>) {
+        val fromClass = mapData["fromClass"].toString().toLowerCase()
+        when (fromClass) {
+            "choosecategorybottomsheetdialogfragment" -> {
+                val chooseCategoryTransaction = mapData["chooseCategory"] as CategoryTransaction
+                text_view_category_activity_add_transaction.text = chooseCategoryTransaction.name
+                text_view_category_activity_add_transaction.tag = chooseCategoryTransaction.id.toString()
+            }
+            else -> {
+                /** nothing to do in here */
+            }
+        }
     }
 }
