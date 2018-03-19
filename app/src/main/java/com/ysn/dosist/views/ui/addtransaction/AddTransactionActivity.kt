d0f09b03508efaea66dc2ac72519e65e34cc5ad9
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-20 00:24:58 +0700

    Add feature formatting amount in Add Transaction Detail

--- app/src/main/java/com/ysn/dosist/views/ui/addtransaction/AddTransactionActivity.kt
+++ app/src/main/java/com/ysn/dosist/views/ui/addtransaction/AddTransactionActivity.kt
@@ -1,8 +1,8 @@
 /*
- * Created by YSN Studio on 3/18/18 10:36 PM
+ * Created by YSN Studio on 3/20/18 12:24 AM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 3/18/18 10:31 PM
+ * Last modified 3/20/18 12:24 AM
  */
 
 package com.ysn.dosist.views.ui.addtransaction
@@ -11,18 +11,25 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.view.View
+import com.jakewharton.rxbinding2.widget.RxTextView
 import com.ysn.dosist.R
 import com.ysn.dosist.db.entity.DetailTransaction
 import com.ysn.dosist.di.component.addtransaction.DaggerAddTransactionActivityComponent
 import com.ysn.dosist.di.module.addtransaction.AddTransactionActivityModule
 import com.ysn.dosist.views.base.BaseActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_add_transaction.*
 import org.greenrobot.eventbus.EventBus
+import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.design.snackbar
+import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import java.text.DecimalFormat
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
-class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickListener {
+class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickListener, AnkoLogger {
 
     @Inject
     lateinit var presenter: AddTransactionPresenter
@@ -37,6 +44,28 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
         text_view_income_activity_add_transaction.setOnClickListener(this)
         text_view_expense_activity_add_transaction.setOnClickListener(this)
         image_view_save_activity_add_transaction.setOnClickListener(this)
+        RxTextView.textChanges(edit_text_amount_add_transaction_activity)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        {
+                            val strAmount = it.toString().replace(",", "")
+                                    .replace(".", "")
+                            if (strAmount.isEmpty()) {
+                                edit_text_amount_add_transaction_activity.setText("0")
+                            } else {
+                                val amount = strAmount.toLong()
+                                val formatAmount = DecimalFormat("#,###").format(amount)
+                                        .replace(",", ".")
+                                edit_text_amount_add_transaction_activity.setText(formatAmount)
+                                edit_text_amount_add_transaction_activity.setSelection(formatAmount.length)
+                            }
+                        },
+                        {
+                            it.printStackTrace()
+                        }
+                )
     }
 
     override fun onError() {
@@ -67,9 +96,11 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
                         .replace(".", "")
                         .toLong()
                 val category = text_view_category_activity_add_transaction.text.toString().toLowerCase()
-                val drawableIncome = ContextCompat.getDrawable(this@AddTransactionActivity, R.drawable.background_rounded_income_active)
-                val drawableIncomeSelected = text_view_income_activity_add_transaction.background
-                val typeTransaction = if (drawableIncomeSelected == drawableIncome) "income" else "expense"
+                // TODO: do something in here (pending)
+
+                val tagIncome = text_view_income_activity_add_transaction.tag
+                val typeTransaction = if (tagIncome == "selected") "income" else "expense"
+                info { "typeTransaction: $typeTransaction" }
                 val detailTransaction = DetailTransaction(
                         subject = subjectTransaction,
                         description = descriptionTransaction,
@@ -90,6 +121,8 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
         text_view_expense_activity_add_transaction.background = null
         text_view_income_activity_add_transaction.background = ContextCompat.getDrawable(this, R.drawable.background_rounded_income_inactive)
         text_view_expense_activity_add_transaction.background = ContextCompat.getDrawable(this, R.drawable.background_rounded_expense_active)
+        text_view_income_activity_add_transaction.tag = "unselected"
+        text_view_expense_activity_add_transaction.tag = "selected"
     }
 
     private fun incomeViewActive() {
@@ -97,6 +130,8 @@ class AddTransactionActivity : BaseActivity(), AddTransactionView, View.OnClickL
         text_view_expense_activity_add_transaction.background = null
         text_view_income_activity_add_transaction.background = ContextCompat.getDrawable(this, R.drawable.background_rounded_income_active)
         text_view_expense_activity_add_transaction.background = ContextCompat.getDrawable(this, R.drawable.background_rounded_expense_inactive)
+        text_view_income_activity_add_transaction.tag = "selected"
+        text_view_expense_activity_add_transaction.tag = "unselected"
     }
 
     override fun getViewContext(): Context = this
