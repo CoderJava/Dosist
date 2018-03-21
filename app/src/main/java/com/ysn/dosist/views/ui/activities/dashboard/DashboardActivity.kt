e1a1eef7b3698b01772aa45c1111c9fe492ef112
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-22 03:27:51 +0700

    Add OnPageChangeListener for view pager in UI Dashboard

--- app/src/main/java/com/ysn/dosist/views/ui/activities/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/ysn/dosist/views/ui/activities/dashboard/DashboardActivity.kt
@@ -1,14 +1,15 @@
 /*
- * Created by YSN Studio on 3/22/18 3:09 AM
+ * Created by YSN Studio on 3/22/18 3:27 AM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 3/22/18 3:08 AM
+ * Last modified 3/22/18 3:26 AM
  */
 
 package com.ysn.dosist.views.ui.activities.dashboard
 
 import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.ViewPager
 import android.view.View
 import com.ysn.dosist.R
 import com.ysn.dosist.di.component.dashboard.DaggerDashboardActivityComponent
@@ -18,6 +19,7 @@ import com.ysn.dosist.views.ui.activities.addtransaction.AddTransactionActivity
 import com.ysn.dosist.views.ui.activities.dashboard.adapter.DashboardPagerAdapter
 import kotlinx.android.synthetic.main.activity_home.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 import org.jetbrains.anko.intentFor
 import javax.inject.Inject
 
@@ -40,6 +42,40 @@ class DashboardActivity : BaseActivity(), DashboardView, View.OnClickListener, A
 
     private fun initListeners() {
         floating_action_button_add_transaction_activity_home.setOnClickListener(this)
+        view_pager_activity_home.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                /* nothing to do in here */
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                /* nothing to do in here */
+            }
+
+            override fun onPageSelected(position: Int) {
+                when (position) {
+                    0 -> tabHomeSelected()
+                    1 -> tabHistoryTransactionSelected()
+                }
+            }
+        })
+    }
+
+    private fun tabHomeSelected() {
+        view_divider_menu_home_active_activity_home.visibility = View.VISIBLE
+        view_divider_menu_history_active_activity_home.visibility = View.INVISIBLE
+        image_view_home_active_activity_home.visibility = View.VISIBLE
+        image_view_home_inactive_activity_home.visibility = View.INVISIBLE
+        image_view_history_active_activity_home.visibility = View.INVISIBLE
+        image_view_history_inactive_activity_home.visibility = View.VISIBLE
+    }
+
+    private fun tabHistoryTransactionSelected() {
+        view_divider_menu_home_active_activity_home.visibility = View.INVISIBLE
+        view_divider_menu_history_active_activity_home.visibility = View.VISIBLE
+        image_view_home_active_activity_home.visibility = View.INVISIBLE
+        image_view_home_inactive_activity_home.visibility = View.VISIBLE
+        image_view_history_active_activity_home.visibility = View.VISIBLE
+        image_view_history_inactive_activity_home.visibility = View.INVISIBLE
     }
 
     override fun onClick(view: View) {
