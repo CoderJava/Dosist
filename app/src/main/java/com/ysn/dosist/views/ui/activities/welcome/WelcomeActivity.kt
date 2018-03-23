ee5c8f2445779e91722d77812d215368b28d0dfd
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-23 21:23:05 +0700

    Create file WelcomeActivity.kt and activity_welcome.xml

---
+++ app/src/main/java/com/ysn/dosist/views/ui/activities/welcome/WelcomeActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * Created by YSN Studio on 3/23/18 9:23 PM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 3/23/18 9:21 PM
+ */
+
+package com.ysn.dosist.views.ui.activities.welcome
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ysn.dosist.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
