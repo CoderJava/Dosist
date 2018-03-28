203459c97fb5846cbacb96611b8ecda610d1455f
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-28 07:56:56 +0700

    Create file AboutActivity.kt and activity_about.xml

---
+++ app/src/main/java/com/ysn/dosist/views/ui/activities/about/AboutActivity.kt
@@ -0,0 +1,20 @@
+/*
+ * Created by YSN Studio on 3/28/18 7:56 AM
+ * Copyright (c) 2018. All rights reserved.
+ *
+ * Last modified 3/28/18 7:56 AM
+ */
+
+package com.ysn.dosist.views.ui.activities.about
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ysn.dosist.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
