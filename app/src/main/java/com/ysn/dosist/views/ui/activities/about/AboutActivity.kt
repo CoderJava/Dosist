84ca88a472e372a2ab69f97aca9fc05287856142
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-03-29 21:00:35 +0700

    Finishing UI About App

--- app/src/main/java/com/ysn/dosist/views/ui/activities/about/AboutActivity.kt
+++ app/src/main/java/com/ysn/dosist/views/ui/activities/about/AboutActivity.kt
@@ -1,20 +1,28 @@
 /*
- * Created by YSN Studio on 3/28/18 7:56 AM
+ * Created by YSN Studio on 3/29/18 9:00 PM
  * Copyright (c) 2018. All rights reserved.
  *
- * Last modified 3/28/18 7:56 AM
+ * Last modified 3/29/18 8:55 PM
  */
 
 package com.ysn.dosist.views.ui.activities.about
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ysn.dosist.BuildConfig
 import com.ysn.dosist.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+        doLoadData()
+    }
+
+    private fun doLoadData() {
+        val versionName = BuildConfig.VERSION_NAME
+        text_view_version_name_activity_about.text = versionName
     }
 }
